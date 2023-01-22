83df694a248de19506be806d2a24c70b3e025571
Iêhudi de Melo Dantas Bezerra <112349582+IehudiMelo@users.noreply.github.com>
2023-01-21 22:50:40 -0300

    Update ISmartphone.java

--- ISmartphone.java
+++ ISmartphone.java
@@ -1,7 +1,18 @@
 public class ISmartphone {
-
-    public static void instalarApp(){
+//função de instalar aplicativos
+    public static Object instalarApp(){
         App app = new App();
+        app.titulo = "Whatsapp";
+        app.fabricante = "Meta";
+        app.tamanhoEmMB = 40000;
+        return app;
+    }
+//função de desinstalar aplicativos
+    public static void desinstalarApp(){
+        return;
+    }
+//função de recuperar aplicativos instalados
+    public static void recuperarAppsInstalados(){
         return;
     }
 }
